ae74e649d451d7cd0d364941dd8cc29f3c472832
puff <yinpuff@gmail.com>
2021-08-21 14:50:17 +0800

    update

---
+++ live-common/common-core/src/main/java/com/live/common/constant/ServiceIdConstant.java
@@ -0,0 +1,16 @@
+package com.live.common.constant;
+
+/**
+ * 服务ID常量
+ * 
+ * kjjjkk
+ *
+ */
+public class ServiceIdConstant {
+	
+	/** 管理服务 */
+	public static final String ADMIN_SERVICE_ID = "live-admin";
+	/** 用户服务 */
+	public static final String USER_SERVICE_ID = "live-user";
+
+}
