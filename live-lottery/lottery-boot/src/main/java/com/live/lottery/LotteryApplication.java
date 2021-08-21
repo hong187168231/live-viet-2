b987077d19aa4eb10ae9f8f6208f9879bbec97d9
puff <yinpuff@gmail.com>
2021-08-21 17:21:51 +0800

    update

--- live-lottery/lottery-boot/src/main/java/com/live/lottery/LotteryApplication.java
+++ live-lottery/lottery-boot/src/main/java/com/live/lottery/LotteryApplication.java
@@ -3,7 +3,9 @@ package com.live.lottery;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients(basePackages = "com.live.*.api")
 @SpringBootApplication
 @EnableDiscoveryClient
 public class LotteryApplication {
